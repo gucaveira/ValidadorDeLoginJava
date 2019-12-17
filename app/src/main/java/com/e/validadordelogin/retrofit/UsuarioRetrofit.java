a852a23c2865053e7a24a17941157ccf959e36ad
gustavo <pereiragustavo251>
2019-12-17 09:43:04 -0300

    configurando o retrofit e add urlBase

---
+++ app/src/main/java/com/e/validadordelogin/retrofit/UsuarioRetrofit.java
@@ -0,0 +1,22 @@
+package com.e.validadordelogin.retrofit;
+
+import com.e.validadordelogin.retrofit.service.UsuarioService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class UsuarioRetrofit {
+
+    public static final String BASE_URL = "https://diego.free.beeceptor.com/";
+    private final UsuarioService usuarioService;
+
+    public UsuarioRetrofit() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+        usuarioService = retrofit.create(UsuarioService.class);
+    }
+
+    public UsuarioService getUsuarioService() {
+        return usuarioService;
+    }
+}
